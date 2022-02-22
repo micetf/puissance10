832264fb6bd5e2f7f8f00729bfc53454c10588fe
MiCetF <webmaster@micetf.fr>
2022-02-22 19:05:38 +0100

    Installation de Babel

--- js/jquery.mailto.js
+++
@@ -1,8 +0,0 @@
-jQuery.fn.mailto=function(qui,chez) {
-	qui='micetf';
-	chez='free';
-	ext='fr';
-	$(this).attr("href",'mailto:'+qui+'@'+chez+'.'+ext);
-	$(this).attr("title",'Pour contacter le webmaster...');
-	return this;
-};	
